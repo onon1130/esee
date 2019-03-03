4db186a204329f6a5f573309e033e811f2d189c9
oscaron <oscaron@10.130.3.46>
2019-03-03 22:36:42 +0800

    swiper css

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
  
     var swiper = new Swiper('.swiper-container', {
       slidesPerView: 'auto',
-      spaceBetween: 30,
+      spaceBetween: 10,
       pagination: {
         el: '.swiper-pagination',
         clickable: true
